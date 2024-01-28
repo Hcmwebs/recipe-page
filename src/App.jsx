6e48d7afd54d8c88a79d0b080fb7da1b15dd35b5
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-29 00:02:55 +1300

    set up the flow and spacing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import { Hero, Ingredients, Instructions, Nutrition, Prep } from './components';
 const App = () => {
 	return (
 		<>
-			<Hero />
-			<Prep />
-			<Ingredients />
-			<Instructions />
-			<Nutrition />
+			<div className='grid grid-cols-1 gap-y-8 lg:gap-y-10 bg-white lg:rounded-3xl'>
+				<header className='lg:p-10'>
+					<figure>
+						<img
+							src='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
+							alt='Shoes'
+							className='w-full lg:rounded-3xl'
+						/>
+					</figure>
+				</header>
+				<main className='grid grid-cols-1 gap-y-8 align-center px-8 lg:p-10 -mt-4 lg:-mt-10'>
+					<Hero />
+					<Prep />
+					<Ingredients />
+					<Instructions />
+					<Nutrition />
+				</main>
+			</div>
 		</>
 	);
 };
