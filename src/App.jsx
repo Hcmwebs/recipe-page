183d6368359f581e37e64784cd88a70810938530
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-29 00:45:12 +1300

    styled the title section

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,16 @@ const App = () => {
 					</figure>
 				</header>
 				<main className='grid grid-cols-1 gap-y-8 align-center px-8 lg:p-10 mt-10 lg:mt-0'>
-					<Hero />
-					<Prep />
-					<Ingredients />
-					<Instructions />
-					<Nutrition />
+					<section className='min-h-30vh grid grid-col-1 gap-y-6'>
+						<h1 className='font-normal text-4xl text-left text-black leading-9'>
+							Simply Omelette recipe
+						</h1>
+						<p className='font-normal text-base text-base-300 leading-6'>
+							An easy and quick dish, perfect for any meal. This classic
+							omelette combines beaten eggs cooked to perfection, optionally
+							filled with your choice of cheese, vegetables, or meats.
+						</p>
+					</section>
 				</main>
 			</div>
 		</>
