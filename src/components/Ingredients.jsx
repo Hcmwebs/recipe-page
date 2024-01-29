d27ddb01df3e72c252ff9969b28ef5afed88dc99
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-30 02:18:43 +1300

    added the left padding to the list

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -5,7 +5,7 @@ const Ingredients = () => {
 	return (
 		<section className='grid grid-cols-1 gap-y-6'>
 			<Title text='ingredients' />
-			<ul className='list-outside list-disc grid gap-y-2'>
+			<ul className='list-outside list-disc grid gap-y-2 pl-4'>
 				{ingredients.map((listItem) => {
 					return <ListItem key={listItem.id} {...listItem} />;
 				})}
