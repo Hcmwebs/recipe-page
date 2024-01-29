edd4d7f47b8f9484199a0986c0a0e79a450a62dc
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-30 10:05:03 +1300

    added some spacing to the Prep component list

--- src/components/Prep.jsx
+++ src/components/Prep.jsx
@@ -5,7 +5,7 @@ const Prep = () => {
 	return (
 		<section className='bg-neutral p-10 grid grid-cols-1 gap-y-4 rounded-xl '>
 			<h2 className='text-xl text-secondary font-semibold'>Preparation time</h2>
-			<ul className='list-outside list-disc'>
+			<ul className='list-outside list-disc grid gap-y-2'>
 				{preparation.map((listItem) => {
 					return <ListItem key={listItem.id} {...listItem} />;
 				})}
