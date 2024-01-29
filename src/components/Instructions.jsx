218da227e4b70af16ff777149bb53f680423a800
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-30 10:52:35 +1300

    changed the color for the list markers

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -5,7 +5,7 @@ const Instructions = () => {
 	return (
 		<section className='grid grid-cols-1 gap-y-6'>
 			<Title text='Instructions' />
-			<ol className='list-outside list-decimal grid gap-y-2 pl-4'>
+			<ol className='marker:text-primary list-outside list-decimal grid gap-y-2 pl-4'>
 				{instructions.map((listItem) => {
 					return <ListItem key={listItem.id} {...listItem} />;
 				})}
