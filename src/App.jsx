0ad0982089a98035caa7123cc4a82934116b424d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-29 21:34:16 +1300

    added more content to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-import { Hero, Ingredients, Instructions, Nutrition, Prep } from './components';
+import {
+	Hero,
+	Ingredients,
+	Instructions,
+	ListItem,
+	Nutrition,
+	Prep,
+} from './components';
+import { ingredients } from './data';
 const App = () => {
 	return (
 		<>
@@ -12,7 +20,7 @@ const App = () => {
 						/>
 					</figure>
 				</header>
-				<main className='w-full grid grid-cols-1 gap-y-8 align-center px-8 lg:p-10 mt-10 lg:mt-0'>
+				<main className='w-full grid grid-cols-1 gap-y-8 lg:gap-y-10 align-center px-8 lg:p-10 mt-10 lg:mt-0'>
 					<section className='grid grid-col-1 gap-y-6'>
 						<h1 className='font-normal text-4xl text-left text-black leading-9'>
 							Simply Omelette recipe
@@ -23,13 +31,31 @@ const App = () => {
 							filled with your choice of cheese, vegetables, or meats.
 						</p>
 					</section>
+					<section className='bg-neutral p-10 grid grid-cols-1 gap-y-4 rounded-xl '>
+						<h2 className='text-xl text-secondary font-semibold'>
+							Preparation time
+						</h2>
+						<ul className='list-outside list-disc'>
+							<li className='pl-4 text-base-300'>
+								<span className='font-bold'>Lorem ipsum: </span>
+								Lorem ipsum dolor sit amet.
+							</li>
+							<li className='pl-4 text-base-300'>
+								<span className='font-bold'>Lorem ipsum: </span>
+								Lorem ipsum dolor sit amet.
+							</li>
+							<li className='pl-4 text-base-300'>
+								<span className='font-bold'>Lorem ipsum: </span>
+								Lorem ipsum dolor sit amet.
+							</li>
+						</ul>
+					</section>
 					<section>
-						<h2 className='text-2xl text-secondary'>Preparation:</h2>
+						<h2>Ingredients</h2>
 						<ul>
-							<li>Lorem ipsum dolor sit amet.</li>
-							<li>Lorem ipsum dolor sit amet.</li>
-							<li>Lorem ipsum dolor sit amet.</li>
-							<li>Lorem ipsum dolor sit amet.</li>
+							{ingredients.map((listItem) => {
+								return <ListItem key={listItem.id} {...listItem} />;
+							})}
 						</ul>
 					</section>
 				</main>
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,8 @@
 import Hero from './Hero';
 import Ingredients from './Ingredients';
 import Instructions from './Instructions';
+import ListItem from './ListItem';
 import Nutrition from './Nutrition';
 import Prep from './Prep';
 
-export { Hero, Prep, Ingredients, Instructions, Nutrition };
+export { Hero, Prep, Ingredients, Instructions, Nutrition, ListItem };
