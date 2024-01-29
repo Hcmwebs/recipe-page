92da46d94b84f647676e10c63cf201ad3b19336d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-30 10:02:01 +1300

    rendered all the card components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,17 +5,21 @@ import {
 	PageIntro,
 	Prep,
 	Ingredients,
+	Divider,
+	Instructions,
 } from './index';
 import { ingredients } from '../data';
 const Card = () => {
 	return (
 		<div className='grid grid-cols-1 bg-white lg:rounded-3xl w-full max-w-[736px] mx-auto'>
 			<Hero />
-			<main className='w-full grid grid-cols-1 gap-y-8 lg:gap-y-10 align-center px-8 lg:p-10 mt-10 lg:mt-0'>
+			<main className='min-h-screen w-full grid grid-cols-1 gap-y-8 lg:gap-y-10 align-center px-8 lg:p-10 mt-10 lg:mt-0'>
 				<PageIntro />
 				<Prep />
 				<Ingredients />
-
+				<Divider />
+				<Instructions />
+				<Divider />
 				<Nutrition />
 			</main>
 		</div>
--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { instructions } from '../data';
+import { Title, ListItem } from './index';
 
 const Instructions = () => {
-	return <h1 className='text-3xl text-center'>instructions goes here</h1>;
+	return (
+		<section className='grid grid-cols-1 gap-y-6'>
+			<Title text='Instructions' />
+			<ol className='list-outside list-decimal grid gap-y-2 pl-4'>
+				{instructions.map((listItem) => {
+					return <ListItem key={listItem.id} {...listItem} />;
+				})}
+			</ol>
+		</section>
+	);
 };
 
 export default Instructions;
